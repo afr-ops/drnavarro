c5197f426f9f81ccabe24ba030d17dcf842f9872
Alan Ruiz <ruizflavio@gmail.com>
2020-11-12 18:16:55 -0300

    arreglo send_mail

--- website/views.py
+++ website/views.py
@@ -4,29 +4,12 @@ from django.conf import settings
 
 from django.core.mail import EmailMultiAlternatives
 
-def send_user_mail(user):
-    subject = 'Titulo del correo'
-    template = get_template('templates/mi_template_correo.html')
-
-    content = template.render({
-        'user': user,
-    })
-
-    message = EmailMultiAlternatives(subject, #Titulo
-                                    ''",
-                 						settings.EMAIL_HOST_USER, #Remitente
-                                    [user.email]) #Destinatario
-
-    message.attach_alternative(content, 'text/html')
-    message.send()
-
-
 
 
 
 def home(request):
 	return render(request, 'home.html', {})
-'''
+
 def contact(request):
 	if request.method == 'POST':
 		#do stuff
@@ -53,4 +36,4 @@ def contact(request):
 		return render(request, 'contact.html', {}) 	
 
 
-	'''
+	
